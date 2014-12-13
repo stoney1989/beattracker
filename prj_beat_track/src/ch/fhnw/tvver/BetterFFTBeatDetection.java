faffd3de110b9a962c26d34b056ca75e6fa7eebe
stoney1989 <nico.hartma@gmail.com>
2014-12-13 13:07:40 +0100

    push some shit on the visualisation shit fuck stack

--- prj_beat_track/src/ch/fhnw/tvver/BetterFFTBeatDetection.java
+++ prj_beat_track/src/ch/fhnw/tvver/BetterFFTBeatDetection.java
@@ -3,6 +3,7 @@ package ch.fhnw.tvver;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -23,82 +24,37 @@ public class BetterFFTBeatDetection extends AbstractBeatTracker {
 
 	@Override
 	public void run() {
-
-		FFT fft = new FFT(1024, 44100);
-		
-		fft.window(FFT.HAMMING); //against spectral leakage
-		
+	
 		float[] sample = new float[1024];		
+		
+		BitSet                beats          = new BitSet();
+		double                freq           = getSampleRate();
+		for(double beat : getRefBeats()) beats.set((int) ((beat) * freq));
+		System.out.println(freq);
+//		int sampleCount = 0;
+
 
 		try {
 			Vis v = new Vis();
 			v.setVisible(true);
+			
+			v.addTextBeat(beats);
+			
+			
+			
 			while (getSamples( sample )) {
 				v.addSample(sample);
-				fft.forward(sample);
-				v.addFFTRE( fft.getSpectrumReal() );
-				//v.repaint();
-//				int mul = 100;
-//				for (int i = 0; i < samples.length; i++) {
-//					int value = (int) (samples[i]*mul);
-//					for (int j = -mul; j <= mul; j++) {
-//						if(value == j){
-//							System.out.print('#');
-//						}else if( j == 0){
-//							System.out.print('|');
-//						}else{
-//							System.out.print(' ');
-//						}
-//						
-//					}
-//					System.out.println();
-//					
-//				}
+				
+				FloatFFT_1D fft_do = new FloatFFT_1D(sample.length);
+				float[] fft = new float[sample.length * 2];
+				System.arraycopy(sample, 0, fft, 0, sample.length);
+				fft_do.realForwardFull( fft );	
+								
+				v.addFFT( fft );
 				
 				
-				//fft.forward( samples );
-			
-				//System.out.println("\n\n");
-				// buffer = new float[ BUFFER_SIZE ];
-				// float[] buffer2 = new float[ BUFFER_SIZE ];
-				//
-				// FloatFFT_1D fft_do = new FloatFFT_1D(BUFFER_SIZE);
-				// float[] fft = new float[BUFFER_SIZE * 2];
-				// System.arraycopy(buffer, 0, fft, 0, BUFFER_SIZE);
-				// fft_do.complexForward( fft );
-				// fft_do.realForward(buffer);
-				//
-				// fft.forward( buffer );
-				//
-				// System.arraycopy( spectrum, 0, lastspectrum, 0,
-				// spectrum.length );
-				// System.arraycopy( fft.getSpectrumReal(), 0, spectrum, 0,
-				// spectrum.length );
-				//
-				// float flux = 0;
-				// for( int i = 0; i < spectrum.length; i++ ){
-				// //flux += ( spectrum[i] - lastspectrum[i] );
-				// }
-				//
-				// //spectralFlux.add( flux );
-				//
-				// System.out.println("-------------------------------------");
-				//
-				// // for (int i = 0; i < BUFFER_SIZE; i++) {
-				// // //System.out.println(buffer[i]);
-				// //// int index = i*2;
-				// //// float re = fft[ index ];
-				// //// float im = fft[ index + 1];
-				// ////
-				// //// spectrum[i] = (float) Math.sqrt( Math.pow( re, 2 ) +
-				// Math.pow( im, 2 ) );
-				// //
-				// //
-				// // System.out.println(spectrum[i]);
-				// // }
-				// //
-				// // if(x>43)break;
-				// // x++;
+				
+//				sampleCount++;
 
 			}
 		} catch (IOException e) {
